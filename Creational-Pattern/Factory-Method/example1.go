6d5fdd41e0771e9aa8b986d417efafa68414f367
mhoarb <mho.arby@gmail.com>
2024-04-08 02:34:53 +0330

    add example1 in factory method

--- Creational-Pattern/Factory-Method/example1.go
+++ Creational-Pattern/Factory-Method/example1.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+type Shape interface {
+	Draw() string
+}
 
+type ShapeFactory interface {
+	CreateShape(shapeType string) Shape
+}
+type Circle struct{}
+
+func (c *Circle) Draw() string {
+	return "0"
+}
+
+type Square struct{}
+
+func (s *Square) Draw() string {
+	return "-----\n|   |\n|   |n-----\n"
 }
