5696af779f44e983cdc593f1151b51bd65214a23
mhoarb <mho.arby@gmail.com>
2024-04-09 15:49:22 +0330

    add factory method example 1

---
+++ Creational-Pattern/Factory-Method/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s\n", g.getName())
+	fmt.Printf("Power: %d\n", g.getPower())
+
+}
