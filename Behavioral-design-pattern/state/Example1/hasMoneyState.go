f5a68f1870eecf2d6a4e1bbd18c5d76c4c18815a
mhoarb <mho.arby@gmail.com>
2024-05-10 16:02:56 +0330

    add other exampls

--- Behavioral-design-pattern/state/Example1/hasMoneyState.go
+++ Behavioral-design-pattern/state/Example1/hasMoneyState.go
@@ -1,25 +1,31 @@
 package main
 
+import "fmt"
+
 type HasMoneyState struct {
 	vendingMachine *VendingMachine
 }
 
-func (h HasMoneyState) addItem(i int) error {
-	//TODO implement me
-	panic("implement me")
+func (h *HasMoneyState) addItem(count int) error {
+	fmt.Printf("%d items added\n", count)
+	h.vendingMachine.incrementItemCount(count)
+	return nil
 }
 
-func (h HasMoneyState) requestItem() error {
-	//TODO implement me
-	panic("implement me")
+func (h *HasMoneyState) requestItem() error {
+	if h.vendingMachine.itemCount == 0 {
+		h.vendingMachine.setState(h.vendingMachine.itemRequested)
+		return fmt.Errorf("no item present")
+	}
+	fmt.Printf("ite requested\n")
+	h.vendingMachine.setState(h.vendingMachine.itemRequested)
+	return nil
 }
 
-func (h HasMoneyState) insertMoney(money int) error {
-	//TODO implement me
-	panic("implement me")
+func (h *HasMoneyState) insertMoney(money int) error {
+	return fmt.Errorf("plesae select item first")
 }
 
-func (h HasMoneyState) dispenseItem() error {
-	//TODO implement me
-	panic("implement me")
+func (h *HasMoneyState) dispenseItem() error {
+	return fmt.Errorf("please select item first")
 }
--- Behavioral-design-pattern/state/Example1/itemRequestedSate.go
+++ Behavioral-design-pattern/state/Example1/itemRequestedSate.go
@@ -1,25 +1,27 @@
 package main
 
+import "fmt"
+
 type ItemRequestedState struct {
 	vendingMachine *VendingMachine
 }
 
-func (i ItemRequestedState) addItem(i2 int) error {
-	//TODO implement me
-	panic("implement me")
+func (i *ItemRequestedState) addItem(count int) error {
+	return fmt.Errorf("item deispense in progress")
 }
 
-func (i ItemRequestedState) requestItem() error {
-	//TODO implement me
-	panic("implement me")
+func (i *ItemRequestedState) requestItem() error {
+	return fmt.Errorf("item already requested")
 }
 
-func (i ItemRequestedState) insertMoney(money int) error {
-	//TODO implement me
-	panic("implement me")
+func (i *ItemRequestedState) insertMoney(money int) error {
+	if money < i.vendingMachine.itemPrice {
+		return fmt.Errorf("inserted money is less. please insert %d", i.vendingMachine.itemPrice)
+	}
+	fmt.Println("money intered is ok")
+	return nil
 }
 
-func (i ItemRequestedState) dispenseItem() error {
-	//TODO implement me
-	panic("implement me")
+func (i *ItemRequestedState) dispenseItem() error {
+	return fmt.Errorf("please insert money first")
 }
